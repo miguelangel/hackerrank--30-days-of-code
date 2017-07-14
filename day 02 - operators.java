ac81b338577b0a239ea96d4b6ef46e5d2a449df5
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-07-14 10:32:26 +0200

    Solved!

--- day 02 - operators.java
+++ day 02 - operators.java
@@ -11,10 +11,13 @@ public class Arithmetic {
         scan.close();
       
         // Write your calculation code here.
+        double tip = mealCost * tipPercent/100;
+        double tax = mealCost * taxPercent/100;
       
         // cast the result of the rounding operation to an int and save it as totalCost 
-        int totalCost = (int) Math.round(/*numberToRoundHere*/);
+        int totalCost = (int) Math.round(mealCost + tip + tax);
       
         // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
     }
 }
