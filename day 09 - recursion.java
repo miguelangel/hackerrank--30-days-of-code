5f2950091ddee6cf7c5713f6d8672181fa08b6b2
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-07-31 16:56:27 +0200

    Solved!

--- day 09 - recursion.java
+++ day 09 - recursion.java
@@ -1,19 +1,15 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     static int factorial(int n) {
-        // Complete this function
+        return n > 1 ? n * factorial(n-1) : 1;
     }
 
     public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-    int result = factorial(n);
-    System.out.println(result);
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int result = factorial(n);
+        System.out.println(result);
     }
 }
