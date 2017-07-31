1f85a0871e16d1e1a46284e6826da58a9330ea01
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-07-31 18:25:17 +0200

    Solved!

--- day 10 - binary numbers.java
+++ day 10 - binary numbers.java
@@ -8,28 +8,12 @@ public class Solution {
         Scanner in =  new Scanner(System.in);
         try {
             int number = in.nextInt();
-    		int curConsecutives = 0; // Current number of consecutive 1s
+    		int curConsecutives = 0; // Current # of consecutive 1s
             while (number > 0) {
-                // Check if last digit is equals to 1
-                if ((number & 1) == 1) {
-                    curConsecutives++;
-                } else {
-                    // Update max consecutive 1s when needed
-                    if (curConsecutives > maxConsecutives) {
-                        maxConsecutives = curConsecutives;
-                    }
-
-                    curConsecutives = 0;
-                }
-
-                // Update last digit of 'number' by dividing by two (right shift)
-                number = number >> 1;
+                curConsecutives = ++curConsecutives * (number & 1); // Update # consecutive 1s by checking with last digit
+                maxConsecutives = Math.max(curConsecutives, maxConsecutives); // Update result
+                number = number >> 1; // Divide by two, updating last digit
             }
-
-            // Update max consecutive 1s when needed
-            if (curConsecutives > maxConsecutives) {
-                maxConsecutives = curConsecutives;
-    		}
         } catch(Exception e) {
             ;
         } finally {
