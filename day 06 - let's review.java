1e823f5595119efcdcd13cf140a4052b5618a6c2
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-07-31 11:35:22 +0200

    Solved!

--- day 06 - let's review.java
+++ day 06 - let's review.java
@@ -1,12 +1,24 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int numberOfStrings = in.nextInt();
+
+        for (int i = 0; i < numberOfStrings; i++) {
+            String currentString = in.next();
+
+            for (int j = 0; j < currentString.length(); j = j+2) {
+                System.out.print(currentString.charAt(j));
+            }
+            System.out.print(" ");
+
+            for (int j = 1; j < currentString.length(); j = j+2) {
+                System.out.print(currentString.charAt(j));
+            }
+
+            System.out.println();
+        }
     }
 }
